7fd1679cc09d68e5646c941739993a48fab240b3
Purnendu Mukherjee <pmukherjee@nvidia.com>
2020-03-07 04:56:30 -0800

    preload trt engine and multiprocess api

--- demo/BERT/python/qa_rest_api.py
+++ demo/BERT/python/qa_rest_api.py
@@ -1,25 +1,37 @@
-from flask import Flask, request, jsonify, json
-import os
+from multiprocessing import Process, Queue
 
-app = Flask(__name__)
+input_queue = Queue()
+output_queue = Queue()
 
-# endpoint to do qa inference and return answer
-@app.route("/qa", methods=["POST"])
-def add_user():
-    try:
-        context = str(request.json['context'])
-        question = str(request.json['question'])
+def run_server(input_queue, output_queue):
 
-        from tr_infer import Model
-        m = Model()
-        r = m.inference(context, question)
+    from flask import Flask, request, jsonify, json
+    app = Flask(__name__)
+    app.run(host='0.0.0.0', port=8880, debug=True)
 
-        return jsonify({"code": 200, "message": r})
+    # endpoint to do qa inference and return answer
+    @app.route("/qa", methods=["POST"])
+    def add_user():
+        try:
+            context = str(request.json['context'])
+            question = str(request.json['question'])
 
-    except Exception as e:
-        return jsonify({"code": 500, "message": type(e).__name__})
+            input_queue.put(context, question)
+            result = output_queue.get()
+
+            return jsonify({"code": 200, "message": result})
+
+        except Exception as e:
+            return jsonify({"code": 500, "message": type(e).__name__})
 
 
 if __name__ == '__main__':
-    # check_db()
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    p = Process(target=run_server, args=(input_queue, output_queue))
+    p.start()
+
+    from tr_infer import Model
+    m = Model()
+    while True:
+        inputs = input_queue.get()
+        r = m.inference(inputs[0], inputs[1])
+        output_queue.put(r)
